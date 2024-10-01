d4edc26bb56670601229b57a9c96857942bf3edf
DsyH75 <badnyaha77777@gmail.com>
2024-10-01 15:30:25 +0300

    Update layout.tsx

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -16,7 +16,7 @@ const inter = Inter({
 const chakra = Chakra_Petch({
   variable: "--font-chakra",
   subsets: ["latin"],
-  weight: ["400", "500", "700"],
+  weight: ["400", "500", "800"],
 });
 
 const metadata: Metadata = {
